0c16d2905569b2cd61e3d9891eac16773c0ca74a
cmy <cmygit@163.com>
2020-09-21 19:42:09 +0800

    24 - 024_尚硅谷_UML类图

---
+++ DesignPattern/src/main/java/org/example/uml/Person.java
@@ -0,0 +1,21 @@
+package org.example.uml;
+
+/**
+ * @Title:
+ * @Author: cmy
+ * @Date: 2020/9/21 19:20
+ */
+public class Person {
+
+    private Integer id;
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
