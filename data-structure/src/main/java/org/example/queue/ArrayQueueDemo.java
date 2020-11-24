a98766585227937fabdfcda07a0e57a459bcad72
cmy <cmygit@163.com>
2020-11-24 21:21:38 +0800

    013-尚硅谷-图解Java数据结构和算法-数组模拟队列代码实现(2)

--- data-structure/src/main/java/org/example/queue/ArrayQueueDemo.java
+++ data-structure/src/main/java/org/example/queue/ArrayQueueDemo.java
@@ -1,5 +1,7 @@
 package org.example.queue;
 
+import java.util.Scanner;
+
 /**
  * @Title:
  * @Author: cmy
@@ -8,7 +10,58 @@ package org.example.queue;
 public class ArrayQueueDemo {
 
     public static void main(String[] args) {
+        // 创建队列
+        ArrayQueue queue = new ArrayQueue(3);
+        // 接收用户输入
+        char key = ' ';
+        Scanner scanner = new Scanner(System.in);
+        boolean loop = true;
+        // 输出一个菜单
+        while (loop) {
+            System.out.println("s(show): 显示队列");
+            System.out.println("e(exit): 退出程序");
+            System.out.println("a(add): 添加数据到队列");
+            System.out.println("p(pop): 弹出队列头的数据");
+            System.out.println("h(head)：获取队列头的数据");
 
+            // 接收一个字符
+            key = scanner.next().charAt(0);
+
+            switch (key) {
+                case 's':
+                    queue.displayQueue();
+                    break;
+                case 'a':
+                    System.out.println("请输入一个数");
+                    int value = scanner.nextInt();
+                    queue.add(value);
+                    break;
+                case 'p':
+                    try {
+                        int result = queue.pop();
+                        System.out.printf("取出的数据是%d\n", result);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'h':
+                    try {
+                        int result = queue.getHead();
+                        System.out.printf("队列头的数据是%d\n", result);
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    }
+                    break;
+                case 'e':
+                    scanner.close();
+                    loop = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        System.out.println("程序退出");
     }
 }
 
@@ -49,13 +102,13 @@ class ArrayQueue {
         return this.front == this.rear;
     }
 
-    public void add(int item) {
+    public void add(int value) {
         if (this.isFull()) {
             throw new RuntimeException("队列已满，不能添加数据~");
         }
 
         this.rear++;
-        this.arr[this.rear] = item;
+        this.arr[this.rear] = value;
     }
 
     public int pop() {
