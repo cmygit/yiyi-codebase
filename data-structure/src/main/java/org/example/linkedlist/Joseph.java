e86501e7f77eb9567bea988953864aa2aad0e313
cmy <cmygit@163.com>
2021-01-05 22:56:27 +0800

    029-尚硅谷-图解Java数据结构和算法-约瑟夫问题分析图解和实现(2)

--- data-structure/src/main/java/org/example/linkedlist/Joseph.java
+++ data-structure/src/main/java/org/example/linkedlist/Joseph.java
@@ -9,8 +9,11 @@ public class Joseph {
 
     public static void main(String[] args) {
         CircleSingleLinkedList list = new CircleSingleLinkedList();
-        list.addBoy(25);
+        int nums = 6;
+        list.addBoy(nums);
         list.show();
+
+        list.countBoy(1, 5, nums);
     }
 }
 
@@ -72,6 +75,63 @@ class CircleSingleLinkedList {
             // 移位
             curBoy = curBoy.getNext();
         }
+    }
+
+    /**
+     * 约瑟夫环 出圈
+     *
+     * @param startNo  表示从第几个小孩开始报数
+     * @param countNum 表示数几下为一轮
+     * @param nums     表示最初有多少个小孩在圈中
+     */
+    public void countBoy(int startNo, int countNum, int nums) {
+        if (this.first == null || startNo < 1 || startNo > nums) {
+            System.out.println("参数输入有误，请重新输入");
+            return;
+        }
+
+        // helper 指向最后一个节点，跟 first 指针同时移动
+        Boy helper = this.first;
+
+        // 初始化 helper，将 helper 指向最后一个节点
+        while (true) {
+            // 判断是否遍历到最后一个节点
+            if (helper.getNext() == this.first) {
+                break;
+            }
+
+            // 移位
+            helper = helper.getNext();
+        }
+
+        // 移动到开始报数的位置
+        for (int i = 0; i < startNo - 1; i++) {
+            this.first = this.first.getNext();
+            helper = helper.getNext();
+        }
+
+        // 报数
+        // 每报数一轮，将 first 指向的节点移出链表（出圈），直到只剩下最后一个节点
+        while (true) {
+            // 判断是否只剩下最后一个节点
+            if (helper == this.first) {
+                break;
+            }
+
+            // 移动一轮
+            for (int i = 0; i < countNum - 1; i++) {
+                this.first = this.first.getNext();
+                helper = helper.getNext();
+            }
+
+            // 出圈
+            System.out.printf("小孩 %d 出圈\n", this.first.getNo());
+            // 重新闭环
+            this.first = first.getNext();
+            helper.setNext(this.first);
+        }
+        // 最后的小孩
+        System.out.printf("最后的小孩 %d\n", this.first.getNo());
     }
 }
 
