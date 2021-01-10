291fd1bbb961eef78bf13811f7396064fc6e7a11
cmy <cmygit@163.com>
2021-01-10 23:21:42 +0800

    040-尚硅谷-图解Java数据结构和算法-中缀转后缀表达式代码实现(1)

--- data-structure/src/main/java/org/example/stack/ReversePolishNotation.java
+++ data-structure/src/main/java/org/example/stack/ReversePolishNotation.java
@@ -1,5 +1,6 @@
 package org.example.stack;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
@@ -12,6 +13,15 @@ import java.util.Stack;
 public class ReversePolishNotation {
 
     public static void main(String[] args) {
+        // 中缀表达式转后缀表达式
+        // 1、1 + ( ( 2 + 3 ) * 4 ) - 5 -> 1 2 3 + 4 * + 5 -
+        String expression = "1+((2+3)*4)-5";
+        List<String> expList = toInfixExpressionList(expression);
+        System.out.println(expList);
+
+    }
+
+    public static void testSuffixExpression() {
         // 逆波兰表达式（后缀表达式）
         // (3+4)*5-6 -> 3 4 + 5 * 6 -
         String suffixExpression = "3 4 + 5 * 6 -";
@@ -21,6 +31,50 @@ public class ReversePolishNotation {
 
         int res = calculate(expressionList);
         System.out.println("res: " + res);
+    }
+
+    /**
+     * 将表达式转为链表
+     *
+     * @param exp 表达式
+     * @return
+     */
+    public static List<String> toInfixExpressionList(String exp) {
+        List<String> expList = new ArrayList<>();
+        // 遍历指针
+        int index = 0;
+        // 保存多位数
+        StringBuilder str = new StringBuilder();
+        // 遍历字符
+        char c;
+
+        while (index < exp.length()) {
+            c = exp.charAt(index);
+
+            // 非数字
+            if (c < 48 || c > 57) {
+                expList.add(String.valueOf(c));
+                index++;
+            } else {
+                // 如果是数字，还需要考虑多位数的情况
+                while (index < exp.length() && c >= 48 && c <= 57) {
+                    str.append(c);
+                    // 移位
+                    index++;
+
+                    if (index < exp.length()) {
+                        // 继续遍历下一个字符
+                        c = exp.charAt(index);
+                    }
+                }
+
+                expList.add(str.toString());
+                // 重置辅助变量
+                str.delete(0, str.length());
+            }
+        }
+
+        return expList;
     }
 
     public static List<String> getExpressionList(String expression) {
