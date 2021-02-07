09a9af8a9fe101c260371554227bd793edb4b7b7
cmy <cmygit@163.com>
2021-02-08 00:22:08 +0800

    110-尚硅谷-图解Java数据结构和算法-堆排序的代码实现2

--- data-structure/src/main/java/org/example/tree/HeapSort.java
+++ data-structure/src/main/java/org/example/tree/HeapSort.java
@@ -1,22 +1,87 @@
 package org.example.tree;
 
+import java.util.Arrays;
+
 /**
- * @Title:
+ * @Title: 堆排序
  * @Author: cmy
  * @Date: 2021/2/6 22:49
  */
 public class HeapSort {
 
     public static void main(String[] args) {
-        int[] arr = {4, 6, 8, 5, 9};
-    }
+        int[] arr = {69, 52, 86, 75, 58, 4, 32, 85, 9, 91};
 
-    public static void sort(int[] arr) {
+        System.out.println("排序前的数组");
+        System.out.println(Arrays.toString(arr));
 
+        sort(arr);
+
+        System.out.println("排序后的数组");
+        System.out.println(Arrays.toString(arr));
     }
 
     /**
-     * 将指定非叶子结点的数调整为大顶堆
+     * 堆排序
+     *
+     * @param arr 待调整数组
+     */
+    public static void sort(int[] arr) {
+        // 最后一个叶子结点
+        int lastNode = arr.length - 1;
+        // 最后一个非叶子结点，即最后一个叶子结点的父结点
+        int parentNode = (lastNode - 1) / 2;
+
+        // 构建最大堆
+        for (int i = parentNode; i >= 0; i--) {
+            heapify(arr, i, arr.length);
+        }
+
+        // 冒泡法+迭代调整堆
+        for (int i = arr.length - 1; i > 0; i--) {
+            // 冒泡
+            swap(arr, 0, i);
+            // 迭代调整堆
+            heapify(arr, 0, i);
+        }
+    }
+
+    /**
+     * 将指定结点对应的子树调整为堆（递归版）
+     *
+     * @param arr 待调整数组（树）
+     * @param k   指定结点
+     * @param n   待调整数组元素个数
+     */
+    public static void heapify(int[] arr, int k, int n) {
+        int max = k;
+        int c1 = 2 * k + 1;
+        int c2 = 2 * k + 2;
+
+        if (c1 < n && arr[c1] > arr[max]) {
+            max = c1;
+        }
+
+        if (c2 < n && arr[c2] > arr[max]) {
+            max = c2;
+        }
+
+        if (max != k) {
+            // 交换
+            swap(arr, k, max);
+            // 交换后，可能需要继续调整对应子树，以满足堆的定义
+            heapify(arr, max, n);
+        }
+    }
+
+    public static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    /**
+     * 将指定结点对应的子树调整为堆（迭代版）
      *
      * @param arr    待调整的数组
      * @param i      非叶子结点在数组中的索引
