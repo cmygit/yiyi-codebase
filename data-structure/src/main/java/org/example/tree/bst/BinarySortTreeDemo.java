14dc48aaabfd2c28f3e762921acc536571a86adf
cmy <cmygit@163.com>
2021-02-13 23:58:30 +0800

    131-尚硅谷-图解Java数据结构和算法-二叉排序树删除叶子结点(2)

--- data-structure/src/main/java/org/example/tree/bst/BinarySortTreeDemo.java
+++ data-structure/src/main/java/org/example/tree/bst/BinarySortTreeDemo.java
@@ -8,7 +8,7 @@ package org.example.tree.bst;
 public class BinarySortTreeDemo {
 
     public static void main(String[] args) {
-        int[] arr = {7, 3, 10, 12, 5, 1, 9};
+        int[] arr = {7, 3, 10, 12, 5, 1, 9,2};
 
         BinarySortTree tree = new BinarySortTree();
 
@@ -19,8 +19,11 @@ public class BinarySortTreeDemo {
         System.out.println("中序遍历：");
         tree.infixOrder();
 
-        Node node = tree.search(99);
-        System.out.println("node = " + node);
+        System.out.println("删除叶子结点~");
+        tree.delNode(2);
+
+        System.out.println("中序遍历：");
+        tree.infixOrder();
     }
 
     private static class BinarySortTree {
@@ -51,6 +54,50 @@ public class BinarySortTreeDemo {
             }
 
             return this.root.search(value);
+        }
+
+        public Node searchParent(int value) {
+            if (this.root == null) {
+                System.out.println("树为空，无法查找");
+                return null;
+            }
+
+            return this.root.searchParent(value);
+        }
+
+        /**
+         * 删除结点
+         *
+         * @param value 目标值
+         */
+        public void delNode(int value) {
+            if (this.root == null) {
+                System.out.println("树为空，无法删除");
+                return;
+            }
+
+            Node targetNode = this.search(value);
+
+            if (targetNode == null) {
+                return;
+            }
+
+            // 如果二叉树只有一个结点
+            if (this.root.left == null && this.root.right == null) {
+                this.root = null;
+                return;
+            }
+
+            Node parentNode = this.searchParent(value);
+
+            // 如果删除的结点是叶子结点
+            if (targetNode.left == null && targetNode.right == null) {
+                if (parentNode.left != null && parentNode.left.value == value) {
+                    parentNode.left = null;
+                } else if (parentNode.right != null && parentNode.right.value == value) {
+                    parentNode.right = null;
+                }
+            }
         }
     }
 
